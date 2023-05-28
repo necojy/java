5a57194803fc5e01525f859074e94f86e69ec1eb
leowujk <110680715+leowujk@users.noreply.github.com>
2023-05-28 13:50:17 +0800

    Update FaceRecognition.java

--- 5.26/FaceRecognition.java
+++ 5.26/FaceRecognition.java
@@ -1,3 +1,4 @@
+package ff.ForFace;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
@@ -9,11 +10,13 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
@@ -57,15 +60,47 @@ public class FaceRecognition extends JButton implements ActionListener {
 
                 Mat cropped = new Mat(src, rect);
 
+                // 銳化裁減的圖片
+                Mat sharpened = new Mat();
+                Imgproc.GaussianBlur(cropped, sharpened, new Size(0, 0), 3);
+                Core.addWeighted(cropped, 1.5, sharpened, -0.5, 0, sharpened);
+
+                // 調整圖片尺寸
+                Mat resized = new Mat();
+                Size newSize = new Size(556, 538);
+                Imgproc.resize(sharpened, resized, newSize);
+
                 String outputFolder = "C:\\output";
                 File folder = new File(outputFolder);
                 if (!folder.exists()) {
                     folder.mkdir();
                 }
 
+             // 繪製圓形
+                int centerX = resized.cols() / 2;
+                int centerY = resized.rows() / 2;
+                int radius = 278;
+                Scalar circleColor = new Scalar(255, 255, 255); // 白色
+
+                // 創建遮罩圖像
+                Mat mask = new Mat(resized.size(), CvType.CV_8UC1, Scalar.all(0));
+                Imgproc.circle(mask, new Point(centerX, centerY), radius, new Scalar(255), -1);
+
+                // 在原圖像上使用遮罩圖像
+                Mat result1 = new Mat();
+                Core.bitwise_and(resized, resized, result1, mask);
+
+                // 將圓外的部分設置為白色
+                Mat outsideMask = new Mat(resized.size(), CvType.CV_8UC1, Scalar.all(255));
+                Core.subtract(outsideMask, mask, outsideMask);
+                Imgproc.cvtColor(outsideMask, outsideMask, Imgproc.COLOR_GRAY2BGR);
+                Core.bitwise_or(result1, outsideMask, result1);
+
+                // 將處理後的圖像保存
                 String outFileName = getUniqueFileName(outputFolder, "test");
-                Imgcodecs.imwrite(outFileName, cropped);
+                Imgcodecs.imwrite(outFileName, result1);
                 System.out.println("Face saved to " + outFileName);
+
             }
             System.out.println("Image Detection finished!");
 
@@ -73,6 +108,7 @@ public class FaceRecognition extends JButton implements ActionListener {
             System.out.println("No file selected.");
         }
     }
+
     private String getUniqueFileName(String folder, String baseName) {
         String extension = ".png";
         String fileName = baseName + count + extension;
